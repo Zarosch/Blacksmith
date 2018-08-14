638a27b87d67a70ff7cf10cc7fcac7d33cb96a23
Kevin <kevin.velz@gmail.com>
2018-08-14 10:00:05 +0200

    add main class

---
+++ src/main/java/me/velz/blacksmith/BlackSmith.java
@@ -0,0 +1,13 @@
+package me.velz.blacksmith;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlackSmith extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    
+}
